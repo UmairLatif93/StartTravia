98daf88720e8a56eea9aa227398211a58373dbec
Umair Latif <m.umair.latif1@gmail.com>
2019-10-09 14:36:00 +0500

    Initial Commit

---
+++ Star Travia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Star Travia
+//
+//  Created by Umair Latif on 09/10/2019.
+//  Copyright © 2019 Umair Latif. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
